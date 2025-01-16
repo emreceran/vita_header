1ed9ae1c5ea0ca2186fdcd109ae056360054349e
emreceran <emrecrn@gmail.com>
2025-01-16 14:33:03 +0300

    carousel ve cards

--- __manifest__.py
+++ __manifest__.py
@@ -18,13 +18,13 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/carousel.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
